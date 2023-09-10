97c9d4cba1367a263db21112d3d1824c130f7716
takuuo2 <126545872+takuuo2@users.noreply.github.com>
2023-09-10 08:40:37 +0000

    create table2

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -2,27 +2,28 @@ class InquiryController < ApplicationController
   require 'httpclient'
 
   def index
-      # client = HTTPClient.new
-      # url = '/inquiries'
-      # response = client.get(url)
-      # @res_json = JSON.parse(response.body)
+      client = HTTPClient.new
+      url = '/inquiries'
+      response = client.get(url)
+      @res_json = JSON.parse(response.body)
   end
 
   def create
     # フォームから送信されたデータをparamsハッシュから取得
-    name = params[:inquiry][:name]
-    email = params[:inquiry][:email]
+    title = params[:inquiry][:title]
+    content = params[:inquiry][:content]
 
     # フォームデータを使ってPOSTリクエストを送信
     client = HTTPClient.new
     url = '/inquiries'
-    post_data = { name: name, email: email }.to_json
+    post_data = { title: title, content: content }.to_json
 
     response = client.post(url, body: post_data, header: { 'Content-Type' => 'application/json' })
 
     if response.status == 200
       # POSTが成功した場合の処理
       @res_json = JSON.parse(response.body)
+      flash[:success] = "POSTリクエストが成功しました。"
       render :index
     else
       # POSTが失敗した場合の処理
